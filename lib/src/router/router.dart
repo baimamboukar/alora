157c2b1cc3a2df7332c6dcb6339d66a448e341a5
baimamboukar <baimamboukar@gmail.com>
2022-07-27 16:08:38 +0100

    added some routes solution payment-confirm

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -3,7 +3,10 @@ import 'package:alora/src/screens/crops_details.dart';
 import 'package:alora/src/screens/crops_view.dart';
 import 'package:alora/src/screens/index.dart';
 import 'package:alora/src/screens/notifications.dart';
+import 'package:alora/src/screens/payment_confirm.dart';
 import 'package:alora/src/screens/predict.dart';
+import 'package:alora/src/screens/purchase.dart';
+import 'package:alora/src/screens/solution.dart';
 import 'package:auto_route/auto_route.dart';
 
 @AdaptiveAutoRouter(replaceInRouteName: 'Page,Route', routes: <AutoRoute>[
@@ -20,5 +23,8 @@ import 'package:auto_route/auto_route.dart';
   AutoRoute(page: Settings, path: '/settings', initial: false),
   AutoRoute(page: Notifications, path: '/notifications', initial: false),
   AutoRoute(page: Predict, path: '/predict/', initial: false),
+  AutoRoute(page: PaymentConfirm, path: '/payment-confirm', initial: false),
+  AutoRoute(page: Purchase, path: '/purchase', initial: false),
+  AutoRoute(page: Solution, path: '/solution', initial: false),
 ])
 class $AppRouter {}
