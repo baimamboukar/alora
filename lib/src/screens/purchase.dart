b6e93f0f687a2fb293e1626aa0da7b5b9bfc683c
baimamboukar <baimamboukar@gmail.com>
2022-07-27 16:09:53 +0100

    purchase screen

---
+++ lib/src/screens/purchase.dart
@@ -0,0 +1,170 @@
+import 'package:alora/src/configs/index.dart';
+import 'package:alora/src/extensions/extensions.dart';
+import 'package:alora/src/widgets/button.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:lottie/lottie.dart';
+
+class Purchase extends StatelessWidget {
+  const Purchase({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Feature> features = const [
+      Feature(name: "Detect diseases on crops", isPrenium: true),
+      Feature(name: "Get Infos on diseases", isPrenium: false),
+      Feature(name: "View the solutions", isPrenium: true),
+      Feature(name: "Bookmark for future read", isPrenium: false),
+      Feature(name: "Acess ressources", isPrenium: true),
+      Feature(name: "Customize app settings", isPrenium: false),
+    ];
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Palette.light.withOpacity(.5), Palette.light],
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ListView(
+              children: <Widget>[
+                const SizedBox(height: 20),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Lottie.asset(
+                      'assets/images/leaves.json',
+                      width: MediaQuery.of(context).size.width,
+                      height: 150,
+                    ),
+                    Column(
+                      children: const [
+                        Center(
+                          child: Text(
+                            'Purchase prenium',
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                color: Palette.primary),
+                          ),
+                        ),
+                        Icon(LineIcons.medal,
+                            color: Colors.orangeAccent, size: 100),
+                      ],
+                    )
+                  ],
+                ),
+                //const SizedBox(height: 4),
+                // const Center(
+                //   child: Text(
+                //     'Coming soon!',
+                //     style: TextStyle(
+                //         fontSize: 16,
+                //         fontWeight: FontWeight.bold,
+                //         color: Colors.white),
+                //   ),
+                // ),
+                //const SizedBox(height: 20),
+                // const Center(
+                //   child: Text(
+                //     'We are working hard to bring you the best experience possible.',
+                //     style: TextStyle(
+                //         fontSize: 16,
+                //         fontWeight: FontWeight.normal,
+                //         color: Palette.dark),
+                //   ),
+                // ),
+                // const SizedBox(height: 20),
+                Card(
+                  elevation: 8.0,
+                  // ignore: sized_box_for_whitespace
+                  child: Container(
+                    width: context.screenWidth * 0.7,
+                    child: Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Features",
+                            style: Styles.designText(
+                                color: Palette.primary, size: 22, bold: true),
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: const [
+                              Text("Basic"),
+                              SizedBox(width: 20),
+                              Text("Prenium")
+                            ],
+                          ),
+                          ...features,
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Button(
+                            callback: () {},
+                            label: "Purchase",
+                            isLoading: false,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Lottie.asset('assets/images/leaves.json'),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class Feature extends StatelessWidget {
+  final String name;
+  final bool isPrenium;
+  const Feature({Key? key, required this.name, required this.isPrenium})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const Icon(
+            LineIcons.dotCircle,
+            size: 20,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(name,
+              style: GoogleFonts.alexBrush(
+                color: Colors.orangeAccent,
+                fontSize: 18,
+              )),
+          Expanded(
+            child: Container(),
+          ),
+          Icon(
+            isPrenium ? LineIcons.timesCircle : LineIcons.checkCircleAlt,
+            color: isPrenium ? Palette.danger : Palette.primary,
+          ),
+          const SizedBox(width: 35),
+          const Icon(
+            LineIcons.checkCircleAlt,
+            color: Palette.primary,
+          ),
+        ],
+      ),
+    );
+  }
+}
