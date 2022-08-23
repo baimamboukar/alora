537080fbf664bca359662d135e930cab729686df
baimamboukar <baimamboukar@gmail.com>
2022-08-23 13:59:07 +0100

    adding lib/src/services/tts/text_to_speech_services.dart {fast_i18n imp} 🚀

--- lib/src/services/tts/text_to_speech_services.dart
+++ lib/src/services/tts/text_to_speech_services.dart
@@ -1,13 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_tts/flutter_tts_web.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class TextToSpeechServices {
   // ignore: unused_field
   final Reader _read;
   TextToSpeechServices(this._read);
-  FlutterTtsPlugin tts = FlutterTtsPlugin();
-  Future<FlutterTtsPlugin> configure(
-      {required String locale, double? volume}) async {
+  FlutterTts tts = FlutterTts();
+  Future<FlutterTts> configure({required String locale, double? volume}) async {
+    await tts.setQueueMode(2);
+    await tts.setVolume(volume ?? 1.0);
+
+    await tts.setLanguage(locale);
     return tts;
   }
 }
