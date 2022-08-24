29456c9f1257f393daf45af976177ffdeb53143e
baimamboukar <baimamboukar@gmail.com>
2022-08-24 12:57:25 +0100

    adding lib/src/widgets/social_button.dart {fixing rename errors} 🚀

--- lib/src/widgets/social_button.dart
+++ lib/src/widgets/social_button.dart
@@ -1,4 +1,4 @@
-import 'package:alora/src/configs/index.dart';
+import 'package:grnagain/src/configs/index.dart';
 import 'package:flutter/material.dart';
 
 class SocialButton extends StatelessWidget {
