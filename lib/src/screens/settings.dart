9250a2ac411647a0483001bc25e68036c1f8cde6
baimamboukar <baimamboukar@gmail.com>
2022-08-25 14:51:18 +0100

    adding lib/src/screens/settings.dart {translated files} 🚀

--- lib/src/screens/settings.dart
+++ lib/src/screens/settings.dart
@@ -2,7 +2,7 @@ import 'package:grnagain/src/extensions/extensions.dart';
 import 'package:grnagain/src/widgets/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:grnagain/i18n/strings.g.dart';
 class Settings extends ConsumerStatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
@@ -22,19 +22,19 @@ class _SettingsState extends ConsumerState<Settings> {
         children: [
           Button(
             isLoading: false,
-            label: context.loc.login,
+            label: t.login,
             callback: () => context.autorouter.pushNamed('/login'),
           ),
           const SizedBox(height: 14),
           Button(
             isLoading: false,
-            label: context.loc.singup,
+            label: t.singup,
             callback: () => context.autorouter.pushNamed('/signup'),
           ),
           const SizedBox(height: 14),
           Button(
             isLoading: false,
-            label: context.loc.profile,
+            label: t.profile,
             callback: () => context.autorouter.pushNamed('/profile'),
           ),
         ],
