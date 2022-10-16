e4ac0e271596bd09c9731302625adb9a47c2322f
baimamboukar <baimamboukar@gmail.com>
2022-10-16 14:16:10 +0100

    adding lib/src/riverpods/auth_riverpods.dart {last modiff} 🚀

--- lib/src/riverpods/auth_riverpods.dart
+++ lib/src/riverpods/auth_riverpods.dart
@@ -3,30 +3,42 @@ import 'package:grnagain/src/services/auth/firebase_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final firebaseAuthRiverpod = Provider<FirebaseAuthentication>((ref) {
-  return FirebaseAuthentication(ref, FirebaseAuth.instance);
-});
+final firebaseAuthRiverpod = Provider<FirebaseAuthentication>(
+  (ref) {
+    return FirebaseAuthentication(ref, FirebaseAuth.instance);
+  },
+);
 final googleAuthRiverpod = Provider<AuthWithGoogle>((ref) {
   return AuthWithGoogle(ref, FirebaseAuth.instance);
 });
-final authStateRiverpod = StreamProvider<User?>((ref) async* {
-  yield* ref.watch(firebaseAuthRiverpod).getAuthUser;
-});
 
-final googleSignInRiverpod = FutureProvider<UserCredential>((
-  ref,
-) async {
-  return ref.read(googleAuthRiverpod).signInWithGoogle();
-});
+final authStateRiverpod = StreamProvider<User?>(
+  (ref) async* {
+    yield* ref.watch(firebaseAuthRiverpod).getAuthUser;
+  },
+);
+
+final googleSignInRiverpod = FutureProvider<UserCredential>(
+  (
+    ref,
+  ) async {
+    return ref.read(googleAuthRiverpod).signInWithGoogle();
+  },
+);
+
 final emailSignInRiverpod =
-    FutureProvider.family<UserCredential?, List<String>>((ref, data) async {
-  return ref
-      .read(firebaseAuthRiverpod)
-      .signupUser(mail: data.first, pass: data.last, name: data.last);
-});
+    FutureProvider.family<UserCredential?, List<String>>(
+  (ref, data) async {
+    return ref
+        .read(firebaseAuthRiverpod)
+        .signupUser(mail: data.first, pass: data.last, name: data.last);
+  },
+);
 
-final googleSignUpRiverpod = FutureProvider<User>((
-  ref,
-) async {
-  return ref.read(googleAuthRiverpod).createAccountWithGoogle();
-});
+final googleSignUpRiverpod = FutureProvider<User>(
+  (
+    ref,
+  ) async {
+    return ref.read(googleAuthRiverpod).createAccountWithGoogle();
+  },
+);
