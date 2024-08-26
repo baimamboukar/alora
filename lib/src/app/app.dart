adaf72398d12beced175aca1703a3be14d98ddff
baimamboukar <baimamboukar@gmail.com>
2024-08-27 00:34:59 +0200

    chore: configure deeplink handler

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -6,6 +8,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:grnagain/i18n/strings.g.dart';
 import 'package:grnagain/src/configs/index.dart';
 import 'package:grnagain/src/router/router.dart';
+import 'package:grnagain/src/router/router.gr.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class GrnAgain extends ConsumerStatefulWidget {
@@ -32,8 +35,25 @@ class _GrnAgainState extends ConsumerState<GrnAgain> {
       builder: (context, Box box, widget) => MaterialApp.router(
         builder: EasyLoading.init(),
         locale: TranslationProvider.of(context).flutterLocale,
-        routerDelegate: appRouter.delegate(),
-        routeInformationParser: appRouter.defaultRouteParser(),
+        routerConfig: appRouter.config(
+          placeholder: (context) => const Center(
+            child: CupertinoActivityIndicator(),
+          ),
+          rebuildStackOnDeepLink: true,
+          deepLinkBuilder: (link) {
+            if (link.path.contains('payment-confirm')) {
+              final args = link.uri.queryParameters;
+              //final invitation = Invitation.fromMap(args, fromInvite: true);
+              return const DeepLink(
+                [
+                  PaymentConfirmRoute(),
+                ],
+              );
+            } else {
+              return DeepLink.defaultPath;
+            }
+          },
+        ),
         restorationScopeId: 'app',
         debugShowCheckedModeBanner: false,
         localizationsDelegates: AppLocalizations.localizationsDelegates,
